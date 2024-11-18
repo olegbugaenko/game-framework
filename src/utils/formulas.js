bf7746dc3b3837160af0b8b457ac54f76c4a5f96
Oleh <oleh.buhaienko@chatbots.studio>
2024-11-18 21:12:37 +0200

    additional exponential param

--- src/utils/formulas.js
+++ src/utils/formulas.js
@@ -1,38 +1,53 @@
 export class Formulas {
 
+    static applyMinMax(val, formula) {
+        if (formula.min != null) {
+            val = Math.max(formula.min, val);
+        }
+        if (formula.max != null) {
+            val = Math.min(formula.max, val);
+        }
+        return val;
+    }
+
     static calculateLinearValue(formula, x) {
-        return formula.B + formula.A * x;
+        let val = formula.B + formula.A * x;
+        return this.applyMinMax(val, formula);
     }
 
     static calculateConst(formula, x) {
-        return x < 1 ? formula.B : formula.A + formula.B;
+        let val = x < 1 ? formula.B : formula.A + formula.B;
+        return this.applyMinMax(val, formula);
     }
 
     static calculateExponentialValue(formula, x) {
-        return formula.B*Math.pow(formula.A, x) + (formula.C || 0);
+        let val = formula.B * Math.pow(formula.A, x) + (formula.C || 0);
+        return this.applyMinMax(val, formula);
     }
 
     static calculatePolyExponential(formula, x) {
-        return formula.B + formula.A * x * Math.pow(formula.C, x);
+        let val = formula.B + formula.A * x * Math.pow(formula.C, x);
+        return this.applyMinMax(val, formula);
     }
 
     static calculateDiminish(formula, x) {
-        return formula.C + formula.A * formula.B * x * Math.pow(x, 0.5) / (formula.B + x);
+        let val = formula.C + formula.A * formula.B * x * Math.pow(x, 0.5) / (formula.B + x);
+        return this.applyMinMax(val, formula);
     }
 
     static calculateValue(formula, x) {
-        if(formula.type === 0) {
-            return Formulas.calculateLinearValue(formula, x)
-        } else if(formula.type === 1) {
-            return Formulas.calculateExponentialValue(formula, x)
-        } else if(formula.type === 2) {
-            return Formulas.calculateConst(formula, x)
-        } else if(formula.type === 3) {
-            return Formulas.calculatePolyExponential(formula, x)
-        } else if(formula.type === 4) {
-            return Formulas.calculateDiminish(formula, x)
+        if (formula.type === 0) {
+            return Formulas.calculateLinearValue(formula, x);
+        } else if (formula.type === 1) {
+            return Formulas.calculateExponentialValue(formula, x);
+        } else if (formula.type === 2) {
+            return Formulas.calculateConst(formula, x);
+        } else if (formula.type === 3) {
+            return Formulas.calculatePolyExponential(formula, x);
+        } else if (formula.type === 4) {
+            return Formulas.calculateDiminish(formula, x);
         } else {
-            throw new Error('Invalid formula type: '+formula.type)
+            throw new Error('Invalid formula type: ' + formula.type);
         }
     }
 
